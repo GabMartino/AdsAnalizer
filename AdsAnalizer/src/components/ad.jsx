9f06ff07b562ee80035daf57be3c08e1173b5cfd
Davide Brunetti <davide.db94@icloud.com>
2020-04-12 18:13:26 +0200

    :lipstick: Updated Ads UIs.

--- AdsAnalizer/src/components/ad.jsx
+++ AdsAnalizer/src/components/ad.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import placeholder from '../assets/images/placeholder.png';
 
+import location from '../assets/icons/pin.png';
+import user from '../assets/icons/user.png';
+
+
 
 class Ad extends Component {
     state = {
@@ -27,31 +31,47 @@ class Ad extends Component {
     }
     render() {
         return (
+
             <div className="ad_tile">
                 <div className="img">
                     <img src={ placeholder } alt="Generic placeholder image"/>
                 </div>
-                <div className="info">
-                    <div className="utils"></div>
-                    <div className="title">
-                        { this.state.title }
+                <div className="content">
+                    <div className="info primary">
+                        <div className="line">
+                            <div className="title">
+                                { this.state.title }
+                            </div>
+                        </div>
+                        <div className="line">
+                            <div className="description">
+                                { this.state.body }
+                            </div>
+                        </div>
                     </div>
-                    <div className="description">
-                        { this.state.body }
-                    </div>
-                    <div className="geographic details">
-                        { this.state.region }
-                        { this.state.province }
-                        { this.state.town }
-                    </div>
-                    <div className="price details">
-                        {this.state.price}
-                    </div>
-                    <div className="author details">
-                        {this.state.author}
+                    <div className="info secondary">
+                        <div className="line">
+                            <div className="location">
+                                <img src={ location } />
+                                { this.state.region }
+                                { this.state.province }
+                                { this.state.town }
+                            </div>
+                            <div className="author">
+                                <img src={ user } />
+                                {this.state.author}
+                            </div>
+                        </div>
+                        <div className="line">
+                            <div className="price">
+                                {this.state.price}
+                            </div>
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buy now</button>
+                        </div>
                     </div>
                 </div>
             </div>
+
          );
     }
 }
