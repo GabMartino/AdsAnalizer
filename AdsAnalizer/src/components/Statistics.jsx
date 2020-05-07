d97dcb4da44e5f180fdf1c9af5762e5e91e0e360
Davide Brunetti <davide.db94@icloud.com>
2020-05-07 02:01:27 +0200

    :lipstick: Styled statistics panel.

--- AdsAnalizer/src/components/Statistics.jsx
+++ AdsAnalizer/src/components/Statistics.jsx
@@ -9,11 +9,11 @@ import consts from '../consts';
 class Statistics extends Component {
 
     //Chart Model parameters
-    
+
     constructor(props){
 
         super(props);
-        
+
         this.fetchData = this.fetchData.bind(this);
         this.setRegion = this.setRegion.bind(this);
         this.setCategory = this.setCategory.bind(this);
@@ -61,13 +61,13 @@ class Statistics extends Component {
                 }
         );
         this.setState({ showSubCategories: true });
-     
+
     }
     setRegion(event){
         event.preventDefault();
         const selectedRegion = Array.isArray(this.state.regions) && this.state.regions.find(region => region._id === event.target.id);
         this.setState({selectedRegion: selectedRegion });
-      
+
     }
     async fetchData(){
         axios.get('http://'+this.props.webServerIP+':'+this.props.webServerPort+'/categories/0'
@@ -97,8 +97,8 @@ class Statistics extends Component {
         let searchFields = null;
         switch(PARAM){
 
-            case consts.STAT_GEO : 
-                                restParam = this.state.selectedSubCategory ? this.state.selectedSubCategory._id : (this.state.selectedCategory ? this.state.selectedCategory._id : null);   
+            case consts.STAT_GEO :
+                                restParam = this.state.selectedSubCategory ? this.state.selectedSubCategory._id : (this.state.selectedCategory ? this.state.selectedCategory._id : null);
                                 if(!restParam) return;
                                 searchFields = {
                                     params: {
@@ -114,7 +114,7 @@ class Statistics extends Component {
                                 break;
 
             case consts.STAT_PRICE_DISTR:
-                                restParam = this.state.selectedCategory ? this.state.selectedCategory._id : null;   
+                                restParam = this.state.selectedCategory ? this.state.selectedCategory._id : null;
                                 if(!restParam) return;
                                 // if is selected the price distribution will be of province
                                 let geoArea = null;
@@ -129,7 +129,7 @@ class Statistics extends Component {
                                 }else{
                                     geoArea = this.state.regions.find( (region) => region.name === searchFieldsFromCharts);
                                 }
-                                
+
                                 searchFields = {
                                     params: {
                                         geo: geoArea._id
@@ -138,8 +138,8 @@ class Statistics extends Component {
                                 this.setState({nameOfPriceDistributionChart: geoArea.name});
                                 this.requireData("categories/" + restParam + "/prices", searchFields, PARAM);
                                 break;
-            
-                                
+
+
 
         }
     }
@@ -147,15 +147,15 @@ class Statistics extends Component {
     async requireData(restParam, searchFields, kindOfStat){
         await axios.get('http://'+this.props.webServerIP+':'+this.props.webServerPort+'/stats/'+restParam, searchFields).then(
             (response) => {
-                  
+
                     switch(kindOfStat){
                         case consts.STAT_GEO:
                                 let xAxis = response.data.map( object => object.name);
                                 let avgPrices = response.data.map( object => object.avg_price);
                                 let numAds = response.data.map( object => object.num_ads);
                                 var x = 0;
-                                while(x < avgPrices.length){ 
-                                    avgPrices[x] = avgPrices[x].toFixed(2); 
+                                while(x < avgPrices.length){
+                                    avgPrices[x] = avgPrices[x].toFixed(2);
                                     x++;
                                 }
                                 this.setState({ yValuesNumberOfAds: numAds});
@@ -167,7 +167,7 @@ class Statistics extends Component {
                                 this.setState({ showPriceDistrChart: true});
                                 this.setState({ histogramData: response.data});
                     }
-                    
+
                 }
         );
     }
@@ -197,11 +197,11 @@ class Statistics extends Component {
         });
     }
     render() {
-     
+
         return (
-            <div >
-                <div>
-                   <h1>Statistics</h1> 
+            <div className="stat_panel">
+                <div className="statistics_title">
+                   <p>Statistics</p>
                 </div>
                 <nav className="navbar nav_2 navbar-expand-lg navbar-light">
                             <DropdownButton id="dropdown-basic-button" className="m-2" title={this.state.selectedCategory != null ? this.state.selectedCategory.name : "Categories"}>
@@ -227,19 +227,19 @@ class Statistics extends Component {
                             </DropdownButton>
                             <button className="btn btn-outline-success my-2 my-sm-0" onClick={e => {e.preventDefault();this.handleSearch(consts.STAT_GEO)}} type="submit">Search</button>
                             <button className="btn btn-outline-success my-2 my-sm-0 m-2" onClick={e => {e.preventDefault();this.cleanSearchFields()}} type="submit">Clear</button>
-                       
+
                 </nav>
-              
+
                 <div >
-                    {this.state.viewCharts ? 
+                    {this.state.viewCharts ?
                         <Tabs defaultActiveKey="first" id="uncontrolled-tab-example" onSelect={this.handleSelect}>
                             <Tab eventKey="first" title="Number Of Ads">
-                                
+
                                 <Tabs defaultActiveKey="barchart" id="kindOfPlot">
                                         <Tab eventKey="barchart" title="BARCHART">
-                                            <BarChart 
+                                            <BarChart
                                                     title = { "Number of Ads" }
-                                                
+
                                                     regionSelected = {this.state.selectedRegion != null}
                                                     yaxis= {this.state.yValuesNumberOfAds}
                                                     xaxis = {this.state.xAxisNumberOfAds}
@@ -264,11 +264,11 @@ class Statistics extends Component {
                                         <Tab eventKey="barchart" title="BARCHART">
                                             <BarChart
                                                     title = { "Average Price" }
-                                                
+
                                                     regionSelected = {this.state.selectedRegion != null}
                                                     yaxis= {this.state.yValuesAveragePrices}
                                                     xaxis = {this.state.xAxisAveragePrices}
-                                                    nameSeries = "Average Price" 
+                                                    nameSeries = "Average Price"
                                                     callStat = {this.handleSearch.bind(this)}
                                             />
                                         </Tab>
@@ -278,23 +278,23 @@ class Statistics extends Component {
                                                     regionSelected = {this.state.selectedRegion != null}
                                                     yaxis= {this.state.yValuesAveragePrices}
                                                     xaxis = {this.state.xAxisAveragePrices}
-                                                    nameSeries = "Average Price" 
+                                                    nameSeries = "Average Price"
                                             />
                                         </Tab> : null }
-                                    
+
 
                                     </Tabs>
                             </Tab>
-                            
+
                         </Tabs> : null}
                     <div className={this.state.showPriceDistrChart ? "" : "notDisplay"}>
                         <HistogramChart
                             title = {"Price Distribution of " + this.state.nameOfPriceDistributionChart}
-                            kindOfChart = "Histogram" 
+                            kindOfChart = "Histogram"
                             data = {this.state.histogramData}
                                />
                     </div>
-                    
+
                 </div>
             </div>
 
